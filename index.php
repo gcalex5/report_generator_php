54b8aa3eb87fcfadcda15d6089b30711e6bba1b2
AlexM <alexmccracken55@gmail.com>
2016-09-22 10:35:14 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexm
+ * Date: 9/21/2016
+ * Time: 11:48 AM
+ */
+echo "<link rel='stylesheet' type='text/css' href='style/style.css' />";
+echo ('<div class="test">Hello World</div>');
