bceb803fb44d9fac02cd587308abc4492028e26a
gcalex5 <alexmccracken55@gmail.com>
2016-09-23 11:27:01 -0400

    setup twig dependency

--- index.php
+++ index.php
@@ -5,5 +5,15 @@
  * Date: 9/21/2016
  * Time: 11:48 AM
  */
-echo "<link rel='stylesheet' type='text/css' href='style/style.css' />";
-echo ('<div class="test">Hello World</div>');
+
+require_once 'lib/composer/vendor/twig/twig/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array(
+  'cache' => 'templates/cache',
+));
+
+
+$template = $twig->loadTemplate('home.twig');
+echo $template->render(array());
