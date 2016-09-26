7b0032bb26384d2e34bf1bd94cbd52a96bdda48f
gcalex5 <alexmccracken55@gmail.com>
2016-09-26 13:09:11 -0400

    - wired up more database functionality - modified templates starting setup on the reps list - some documentation

--- index.php
+++ index.php
@@ -1,29 +1,72 @@
 <?php
 /**
- * Created by PhpStorm.
+ *
+ * Handles instancing of Twig and recieving the Ajax POST request to generate specific reports
+ *
  * User: alexm
  * Date: 9/21/2016
  * Time: 11:48 AM
  */
 
+include 'src/tools/queries.php';
+include 'src/reports/ElectricitySummary.php';
 require_once 'lib/composer/vendor/twig/twig/lib/Twig/Autoloader.php';
 
+/**
+ * If we have no Twig instance create it here.
+ * Setup the database connection
+ */
 if(!isset($twig)){
   Twig_Autoloader::register();
-
   $loader = new Twig_Loader_Filesystem('templates');
   $twig = new Twig_Environment($loader, array(
     'cache' => 'templates/cache',
   ));
-
   $template = $twig->loadTemplate('index.html.twig');
+
+  //Disable caching for development
   echo $template->render(array('cache' => false, 'auto_reload' => true));
+
+  //Database connection
+  $conn = init_database();
+  if (!mysqli_ping($conn)) {
+    //TODO: check connection and re-attempt then display error
+    echo('Failed connection');
+    exit;
+  }
 }
 
-if(isset($_POST['report']) && !empty($_POST['report'])){
-  
+/**
+ * If we have a database connection, carry on and
+ * Query/Generate the required fields for the form
+ */
+if(isset($conn)){
+  //Reps
+  //Min/Max date fields
+
+
 }
 
-else{
+/**
+ * If we have a requested report, start here
+ */
+if(isset($conn) && (isset($_POST['report']) && !empty($_POST['report']))) {
+    //Call to the specific report constructors
+    //TODO: Send date barriers and rep list to report constructors
+    if($_POST['report'] == 'electric'){
+      $report = new ElectricitySummary();
+    }
 
+    if($_POST['report'] == 'natural_gas'){
+      $report = new NaturalGasSummary();
+    }
+
+    if($_POST['report'] == 'commission'){
+      $report = new MonthlyCommission();
+    }
+
+    if($_POST['report'] == 'book'){
+      $report = new BookOfBusiness();
+    }
+    //TODO: convert outputted report data to JSON and send to frontend
 }
---
+++ src/tools/queries.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 9/26/16
+ * Time: 12:13 PM
+ */
+
+function init_database(){
+  $config_array = parse_ini_file("config/database.ini");
+  $user = $config_array['username'];
+  $pass = $config_array['password'];
+  $url = $config_array['url'];
+  $db = $config_array['db'];
+  $conn = new mysqli($url, $user, $pass, $db);
+  if($conn->connect_error){
+    //TODO: have this return an error to 'index.php' and display the database connection issue
+    die("Connection failed: " . $conn->connect_error);
+  }
+  else{
+    return $conn;
+  }
+}
+
+function query_reps($conn){
+  $query = 'SELECT ID, First, Last, Title FROM reps';
+  $result = $conn->mysql_query($query);
+  return $result;
+}
